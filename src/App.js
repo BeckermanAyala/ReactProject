c9bb6b99471142531ff7c81be59ac1fc6374f32e
BeckermanAyala <a0533190528@gmail.com>
2024-04-10 15:20:38 +0300

    i add my project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './Project/redux/reducers/Store';
+import NavbarRoute from './Project/NavbarRoute';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+<Provider store={store}>
+<NavbarRoute></NavbarRoute>
+
+</Provider> 
+        
       </header>
     </div>
   );
